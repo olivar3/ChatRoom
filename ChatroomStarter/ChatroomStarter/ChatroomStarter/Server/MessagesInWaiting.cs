670bbadff4af6e5a73e342739bbb4c12ac3b30cf
olivar3 <olivar3@outlook.com>
2017-08-24 08:32:01 -0500

    messages in waiting on client side was deleted and new one created on server side. the classes in the server side were made public for now.

---
+++ ChatroomStarter/ChatroomStarter/ChatroomStarter/Server/MessagesInWaiting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class MessagesInWaiting
+    {
+        public MessagesInWaiting()
+        {
+
+        }
+        public void LastMessageEntered()
+        {
+
+        }
+        public void RecordOfMessages()
+        {
+
+        }
+        public void NextMessage()
+        {
+
+        }
+    }
+}
